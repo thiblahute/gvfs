19fa7f6535ecfd80e9c1072663423d56f58e6a83
thiblahute <saunierthibault@gmail.com>
2009-07-07 16:50:17 -0400

    rename google_documents to gdocs

---
+++ daemon/gvfsbackendgdocs.c
@@ -0,0 +1,209 @@
+/* GIO - GLib Input, Output and Streaming Library
+ * 
+ * Copyright (C) 2008 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: Thibault Saunier <saunierthibault@gmail.com>
+ */
+
+#include <config.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#include <glib/gstdio.h>
+#include <glib/gi18n.h>
+#include <gio/gio.h>
+
+#include "gvfsbackendgdocs.h"
+#include "gvfsjobopenforread.h"
+#include "gvfsjobread.h"
+#include "gvfsjobseekread.h"
+#include "gvfsjobopenforwrite.h"
+#include "gvfsjobwrite.h"
+#include "gvfsjobseekwrite.h"
+#include "gvfsjobsetdisplayname.h"
+#include "gvfsjobqueryinfo.h"
+#include "gvfsjobqueryfsinfo.h"
+#include "gvfsjobqueryattributes.h"
+#include "gvfsjobenumerate.h"
+#include "gvfsdaemonprotocol.h"
+#include "gvfskeyring.h"
+
+#include "soup-input-stream.h"
+
+
+G_DEFINE_TYPE (GVfsBackendGdocs, g_vfs_backend_gdocs, G_VFS_TYPE_BACKEND)
+
+static void
+g_vfs_backend_gdocs_finalize (GObject *object)
+{
+  GVfsBackendGdocs *backend;
+
+  backend = G_VFS_BACKEND_GDOCS (object);
+}
+
+static void
+g_vfs_backend_gdocs_dispose (GObject *object)
+{
+  GVfsBackendGdocs *backend;
+
+  backend = G_VFS_BACKEND_GDOCS (object);
+  if (backend->service != NULL )
+	  g_object_unref (backend->service);
+}
+
+#define DEBUG_MAX_BODY_SIZE (100 * 1024 * 1024)
+
+static void
+g_vfs_backend_gdocs_init (GVfsBackendGdocs *backend)
+{
+  const char         *debug;
+  backend->service =  gdata_documents_service_new (CLIENT_ID);
+
+  /* Gnome proxy configuration is handle by libgdata*/
+
+  /* Logging */
+  debug = g_getenv ("GVFS_GDOCS_DEBUG");
+  if (debug)
+    {
+		/*TODO*/
+    }
+
+}
+
+/* ************************************************************************* */
+/* public utility functions */
+
+/* ************************************************************************* */
+/* virtual functions overrides */
+static void
+do_mount (GVfsBackend *backend,
+          GVfsJobMount *job,
+          GMountSpec *mount_spec,
+          GMountSource *mount_source,
+          gboolean is_automount)
+{
+	GVfsBackendGdocs *gdocs_backend = G_VFS_BACKEND_GDOCS (backend);
+	gchar *username, *password, *prompt=NULL, *display_name;
+	gboolean aborted, retval, save_password=FALSE;
+	GPasswordSave password_save = G_PASSWORD_SAVE_NEVER;
+	GAskPasswordFlags flags;
+	gint mount_try;
+	GError *error = NULL;
+
+	username = g_strdup (g_mount_spec_get (mount_spec, "user"));
+
+	/*Set the password asking flag and prompt message*/
+	prompt = g_strdup_printf (_("Enter password to access %s's google documents."), username);
+	flags = G_ASK_PASSWORD_NEED_PASSWORD;
+	if (g_vfs_keyring_is_available ())
+		flags |= G_ASK_PASSWORD_SAVING_SUPPORTED;
+	if (username == NULL)
+	    flags |= G_ASK_PASSWORD_NEED_USERNAME;
+
+	/*Check if the password as already been saved for the user*/
+	if (!g_vfs_keyring_lookup_password (gdata_service_get_username (GDATA_SERVICE (gdocs_backend->service)), NULL,
+																	NULL, "gdata", NULL, NULL, 0, &username, NULL,
+																   	&password))
+	{
+
+		if (!g_mount_source_ask_password (mount_source, prompt, username, NULL, flags, &aborted, &password, &username,	NULL, FALSE,
+										  &password_save) || aborted)
+				g_vfs_job_failed (G_VFS_JOB (job), G_IO_ERROR, G_IO_ERROR_PERMISSION_DENIED,
+								  _("Password dialog cancelled"));
+		else
+			save_password = TRUE;
+		
+    }
+
+	mount_try = 0;
+	/*Try to connect to the server*/
+	while (retval == FALSE || mount_try != 3) /*TODO find something better than 3 tries hard coded*/
+	{
+		retval = gdata_service_authenticate (GDATA_SERVICE (gdocs_backend->service), username, password, NULL, &error);
+		if (retval == TRUE)
+		{
+			/*save the password*/
+			if (save_password == TRUE)
+				g_vfs_keyring_save_password (gdata_service_get_username ( GDATA_SERVICE (gdocs_backend->service)), NULL, NULL, "gdata", NULL, NULL, 0,
+										 gdata_service_get_password (GDATA_SERVICE (gdocs_backend->service)), password_save);
+	
+			/*Mount it*/
+			mount_spec = g_mount_spec_new ("gdocs");
+			display_name = g_strdup_printf (_("%s on google documents"), gdata_service_get_username (GDATA_SERVICE (gdocs_backend->service)));
+
+			g_vfs_backend_set_mount_spec (backend, mount_spec);
+			g_mount_spec_unref (mount_spec);
+
+			g_vfs_backend_set_display_name (backend, display_name);
+			g_free (display_name);
+			g_vfs_backend_set_icon_name (backend, "folder-remote");
+		}
+		else
+		{
+			if (!g_mount_source_ask_password (mount_source, prompt, username, NULL, flags, &aborted, &password, &username,	NULL, FALSE,
+										  &password_save) || aborted)
+				g_vfs_job_failed (G_VFS_JOB (job), G_IO_ERROR, G_IO_ERROR_PERMISSION_DENIED, _("Password dialog cancelled"));
+			else
+				save_password = TRUE;
+			mount_try++;
+		}
+	}
+    if (retval == FALSE)
+		g_vfs_job_failed (G_VFS_JOB (job), G_IO_ERROR, G_IO_ERROR_FAILED, _("Failed to mount google documents"));
+	g_free (prompt);
+	g_free (username);
+	g_free (password);
+}
+
+
+void do_move (GVfsBackend *backend, GVfsJobMove *job, const char *source, const char *destination, GFileCopyFlags flags,
+		   GFileProgressCallback progress_callback, gpointer progress_callback_data)
+{
+	GVfsBackendGdocs *gdocs_backend = G_VFS_BACKEND_GDOCS (backend);
+	GDataDocumentsQuery *query;
+	GDataDocumentsEntry *source_entry;
+	GDataDocumentsFolder *destination_folder;
+
+		
+}
+
+
+static void
+g_vfs_backend_gdocs_class_init (GVfsBackendGdocsClass *klass)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+  GVfsBackendClass *backend_class;
+  
+  gobject_class->finalize  = g_vfs_backend_gdocs_finalize;
+  gobject_class->dispose  = g_vfs_backend_gdocs_dispose;
+
+  backend_class = G_VFS_BACKEND_CLASS (klass); 
+
+  backend_class->mount         = do_mount;
+/*  backend_class->try_open_for_read = try_open_for_read;
+  backend_class->try_read          = try_read;
+  backend_class->try_seek_on_read  = try_seek_on_read;
+  backend_class->try_close_read    = try_close_read;
+  backend_class->try_query_info    = try_query_info;
+*/
+}
